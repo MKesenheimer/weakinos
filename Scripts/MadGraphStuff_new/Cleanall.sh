0b2e96ba06d4d261ace6945afb569474a21cd348
MKesenheimer <mattoef@yahoo.de>
2016-01-15 11:50:27 +0100

    MadGraphStuff_new Update

--- Scripts/MadGraphStuff_new/Cleanall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-\rm -rf MadTMP
-
-\rm -rf `cat MGfiles.list`
-
--- Scripts/MadGraphStuff_new/pwhg_analysis-dummy.f
+++
@@ -1,6 +0,0 @@
-      subroutine init_hist
-      end
-
-      subroutine analysis(dsig)
-      real * 8 dsig
-      end
